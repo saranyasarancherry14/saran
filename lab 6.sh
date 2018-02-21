700be4e7d85f55d7c9217e637c75f34a3ac7ae4d
saranyasarancherry14 <35552361+saranyasarancherry14@users.noreply.github.com>
2018-02-21 10:22:30 +0530

    Update lab 6.sh

--- lab 6.sh
+++ lab 6.sh
@@ -2,19 +2,23 @@ echo input a
 read a
 echo input b
 read b
+echo 1.add 2.sub 3.mul 4.dev 5.mod
 echo operator
 read o
 case $o in
-	+)
+	1)
 		echo `expr $a + $b`
 		;;
-	-)
+	2)
 		echo `expr $a - $b`
 		;;
-       *)
+       3)
 	       echo `expr $a \* $b`
     	       ;;
-	/)
+	4)
 		echo `expr $a / $b`
 		;;
+	5)
+	       echo `expr $a % $b`
+	       ;;
 esac
