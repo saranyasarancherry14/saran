849ddcf11d7a0753cd12f802a0861fd55b9e8bf6
saranyasarancherry14 <35552361+saranyasarancherry14@users.noreply.github.com>
2018-02-22 08:48:38 +0530

    Add files via upload

---
+++ lab5.sh
@@ -0,0 +1,14 @@
+echo "enter a number: "
+read num
+i=2
+while [$i -1t $num]
+do
+	if [`expr $num % $i ` -eq 0]
+	then
+		echo"$num is not a prime number"
+		echo "since it is divisible by $i"
+		exit
+	fi
+	i= `expr $i + 1`
+done
+echo "$num is a prime number "
