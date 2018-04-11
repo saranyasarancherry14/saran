6acd54b3499e6a49eda4ec190e692b3f16cc0a7f
saranyasarancherry14 <35552361+saranyasarancherry14@users.noreply.github.com>
2018-04-11 09:58:48 +0530

    Add files via upload

---
+++ lab17.sh
@@ -0,0 +1,7 @@
+
+ught  of the day*
+~~~~~~~~~~~~~~~~~~~~~~
+quotes=("Work is worship" "Try and try till u succeed" "Make hay while sunshines")
+quote=${quotes[$RANDOM % ${#quotes[@]}]}
+figlet $quote
+echo -e "\n"
